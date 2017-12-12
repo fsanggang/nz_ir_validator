b89b2f94caf9825caf2bcadce3565ce9c9e343a5
Fiona Sanggang <fsanggang@gmail.com>
2017-12-13 08:43:47 +1300

    Initial commit

---
+++ lib/nz_ir_validator.ex
@@ -0,0 +1,18 @@
+defmodule NzIrValidator do
+  @moduledoc """
+  Documentation for NzIrValidator.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NzIrValidator.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nz_ir_validator_test.exs
@@ -0,0 +1,8 @@
+defmodule NzIrValidatorTest do
+  use ExUnit.Case
+  doctest NzIrValidator
+
+  test "greets the world" do
+    assert NzIrValidator.hello() == :world
+  end
+end
