429396c1b4860fcb755abbe7ed48f6bf0401dfa8
Fiona Sanggang <fsanggang@gmail.com>
2017-12-14 21:27:07 +1300

    First cut of NzIrValidator

--- lib/nz_ir_validator.ex
+++ lib/nz_ir_validator.ex
@@ -3,16 +3,64 @@ defmodule NzIrValidator do
   Documentation for NzIrValidator.
   """
 
+  @length 8
+  @primary_weighting [ 3, 2, 7, 6, 5, 4, 3, 2 ]
+  @secondary_weighting [ 7, 4, 3, 2, 5, 2, 7, 6 ]
+  @modulus 11
+
   @doc """
-  Hello world.
+  Validate the provided IR number.
 
   ## Examples
 
-      iex> NzIrValidator.hello
-      :world
+    iex> NzIrValidator.is_valid?(49091850)
+    {:ok, true}
+
+    iex> NzIrValidator.is_valid?(9125568)
+    {:error, false}
 
   """
-  def hello do
-    :world
+  @spec is_valid?( integer ) :: { :ok, true } | { :error, false }
+  def is_valid?( ird_number ) when not ird_number in 10_000_000..150_000_000, do: { :error, false }
+  def is_valid?( ird_number ) do
+    as_list = Integer.digits( ird_number )
+    base    = Enum.drop( as_list, -1 ) |> Integer.undigits
+    check   = List.last( as_list )
+
+    base |> pad |> as_integer_list |> apply_algorithm( @primary_weighting, check )
   end
+
+  # Calls helper functions to determine the validity of an IR number
+  @spec apply_algorithm( list( integer ), list( integer ), integer ) :: { :ok, true } | { :error, false }
+  defp apply_algorithm( list, weighting, check ) do
+    list |> apply_weighting( weighting ) |> Enum.sum |> rem( @modulus) |> compare_against_check_digit( check, list )
+  end
+
+  # Pads an IR number with leading zeroes if necessary.
+  # The required number of digits is specified by @length
+  @spec pad( integer ) :: String.t
+  defp pad( base ) do
+    Integer.to_string( base ) |> String.pad_leading( @length, "0" )
+  end
+
+  # Converts an IR number from a String into an integer list.
+  @spec as_integer_list( String.t ) :: list( integer )
+  defp as_integer_list( padded ) do
+    String.split( padded, "", trim: true ) |> Enum.map( fn(x) -> String.to_integer(x) end )
+  end
+
+  # Multiply each integer in the list with its corresponding weighting, as
+  # specified in @primary_weighting and @secondary_weighting.
+  @spec apply_weighting( list( integer ), list( integer ) ) :: list( integer )
+  defp apply_weighting( list, weighting ) do
+    Enum.zip( list, weighting ) |> Enum.reduce( [], fn(x, acc) -> acc ++ [elem(x, 0) * elem(x, 1)] end )
+  end
+
+  # Compare the remainder against the check digit.
+  # Re-apply the validation algorithm (apply_algorithm()) if necesssary
+  @spec compare_against_check_digit( list( integer), integer, integer ) :: { :ok, true } | { :error, false }
+  defp compare_against_check_digit( remainder, _check, _list ) when remainder == 0, do: { :ok, true }
+  defp compare_against_check_digit( remainder, check, _list ) when @modulus - remainder == check, do: { :ok, true }
+  defp compare_against_check_digit( remainder, check, list ) when @modulus - remainder == 10, do: apply_algorithm( list, @secondary_weighting, check )
+  defp compare_against_check_digit( _remainder, _check, _list ), do: { :error, false }
 end
--- test/nz_ir_validator_test.exs
+++ test/nz_ir_validator_test.exs
@@ -2,7 +2,27 @@ defmodule NzIrValidatorTest do
   use ExUnit.Case
   doctest NzIrValidator
 
-  test "greets the world" do
-    assert NzIrValidator.hello() == :world
+  test "identifies valid ir number 49091850" do
+    assert NzIrValidator.is_valid?(49091850) == {:ok, true}
+  end
+
+  test "identifies valid ir number 35901981" do
+    assert NzIrValidator.is_valid?(35901981) == {:ok, true}
+  end
+
+  test "identifies valid ir number 49098576" do
+    assert NzIrValidator.is_valid?(49098576) == {:ok, true}
+  end
+
+  test "identifies valid ir number 136410132" do
+    assert NzIrValidator.is_valid?(136410132) == {:ok, true}
+  end
+
+  test "identifies invalid ir number 136410133" do
+    assert NzIrValidator.is_valid?(136410133) == {:error, false}
+  end
+
+  test "identifies invalid ir number 9125568" do
+    assert NzIrValidator.is_valid?(9125568) == {:error, false}
   end
 end
